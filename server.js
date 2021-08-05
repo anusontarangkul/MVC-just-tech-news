979c9a09439bdc2baf4f9c4dd9587f6e0a454668
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-08-04 18:37:38 -0700

    add login/logout

--- server.js
+++ server.js
@@ -9,7 +9,19 @@ const hbs = exphbs.create({})
 const app = express();
 const PORT = process.env.PORT || 3002;
 
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,5 +33,5 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 sequelize.sync({ force: true }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
